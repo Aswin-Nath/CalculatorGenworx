8a526841047007b49f2d2d68f1dfea3fc024fcae
Srinath <srisrisri044@gmail.com>
2025-08-22 14:22:37 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from add import add
 from sub import sub
+from div import div
 
-ans = add(4, 5)
-print(ans)
+while True:
+    ch = input("Enter the choice: 1.Add\n2.Sub\n3.Mul\n4.Div\n5.Exit")
+    if ch == 1:
+        pass
+    elif ch == 2:
+        pass
+    elif ch == 3:
+        pass
+    elif ch == 4:
+        pass
+    elif ch == 5:
+        pass
+    else:
+        print("Invalid choice")
