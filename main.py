4cc620172b064c920ce5deb57362d067712a1f4c
Srinath <srisrisri044@gmail.com>
2025-08-22 14:26:58 +0530

    add and sub done

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from div import div
 while True:
     ch = input("Enter the choice: 1.Add\n2.Sub\n3.Mul\n4.Div\n5.Exit")
     if ch == 1:
-        pass
+        inp = list(map(int, input().split()))
+        print(add(inp))
     elif ch == 2:
-        pass
+        inp = list(map(int, input().split()))
+        print(sub(inp))
     elif ch == 3:
         pass
     elif ch == 4:
